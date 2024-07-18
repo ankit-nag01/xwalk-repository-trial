093b93d1f54b5c9c4f66a20dec3bbb44bf123b46
ankit-nag01 <ankit01@nagarro.com>
2024-07-18 11:38:30 +0530

    update

---
+++ blocks/banner/banner.js
@@ -0,0 +1,52 @@
+export default function decorate(block){
+    function getBannerCard(){
+        const[
+            pretitleEl,
+            titleEl,
+            carImageEl,
+            priceEl,
+            priceTypeEl,
+            ctaText1El,
+            ctaLink1El,
+            ctaTarget1El,
+            ctaText2El,
+            ctaLink2El,
+            ctaTarget2El
+        ] = block.children
+
+        const pretitle = pretitleEl?.textContent?.trim() || "";
+        const title = titleEl?.textContent?.trim() || "";
+
+        const carImageElement = carImageEl.querySelector('img');
+        const carImage = carImageElement.getAttribute('src').trim() || "";
+    
+        const price = priceEl?.textContent?.trim() || "";
+        const priceType = priceTypeEl?.textContent?.trim() || "";
+
+        console.log("my type value is", priceType);
+        const ctatextFirst = ctaText1El?.textContent?.trim() || "";
+        const ctaLinkFirst = ctaLink1El?.textContent?.trim() || "";
+        const ctaTaregetFirst = ctaTarget1El?.textContent?.trim() || "";
+        const ctaTextSecond = ctaText2El?.textContent?.trim() || "";
+        const ctaLinkSecond = ctaLink2El?.textContent?.trim() || "";
+        const ctaTargetSecond = ctaTarget2El?.textContent?.trim() || "";
+
+        return{
+        price,
+        priceType,
+        ctatextFirst,
+        ctaLinkFirst,
+        ctaTaregetFirst,
+        ctaTextSecond,
+        ctaLinkSecond,
+        ctaTargetSecond
+        };
+
+    }
+
+    const bannerBar = getBannerCard(block);
+
+    const bannerHTML = `
+
+    `
+}
