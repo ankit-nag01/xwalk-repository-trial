99f0514f2d3a1fbd874334e8d75fc8ded6d7af9d
ankit-nag01 <ankit01@nagarro.com>
2024-07-18 13:33:06 +0530

    update

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -16,22 +16,23 @@ export default function decorate(block){
 
         const pretitle = pretitleEl?.textContent?.trim() || "";
         const title = titleEl?.textContent?.trim() || "";
-
         const carImageElement = carImageEl.querySelector('img');
         const carImage = carImageElement.getAttribute('src').trim() || "";
     
         const price = priceEl?.textContent?.trim() || "";
+        console.log("my price value is",priceEl);
         const priceType = priceTypeEl?.textContent?.trim() || "";
-
-        console.log("my type value is", priceType);
         const ctatextFirst = ctaText1El?.textContent?.trim() || "";
         const ctaLinkFirst = ctaLink1El?.textContent?.trim() || "";
         const ctaTaregetFirst = ctaTarget1El?.textContent?.trim() || "";
         const ctaTextSecond = ctaText2El?.textContent?.trim() || "";
         const ctaLinkSecond = ctaLink2El?.textContent?.trim() || "";
         const ctaTargetSecond = ctaTarget2El?.textContent?.trim() || "";
-
+       
         return{
+        pretitle,
+        title,
+        carImage,
         price,
         priceType,
         ctatextFirst,
@@ -41,9 +42,9 @@ export default function decorate(block){
         ctaLinkSecond,
         ctaTargetSecond
         };
-
+        
     }
-
+    
     const bannerBar = getBannerCard(block);
 
     const bannerHTML = `
